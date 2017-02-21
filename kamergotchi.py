319023ba8a410e2100c75bdc6223cac7044ec01a
Norbert vA <norbert@nobert.in>
2017-02-21 10:21:55 +0100

    Added 'claims', added exception for consumptions, added timestamps

--- kamergotchi.py
+++ kamergotchi.py
@@ -19,32 +19,51 @@ consumptions = ['food', 'attention', 'knowledge']
 def giveConsumption():
     typeNom = random.choice(consumptions)
     contentNom = '{"bar":"'+str(typeNom)+'"}'
-    nomnomnom = requests.post(baseUrl+"/care/", headers=reqHeadersPost, data=contentNom)
-    jsonObj = nomnomnom.json()
-    print("Giving "+str(typeNom))
+    nomnomnom = requests.post(baseUrl+"/care", headers=reqHeadersPost, data=contentNom)
+    nomStamp = datetime.datetime.now()
+    print(str(nomStamp)+" Giving "+str(typeNom))
+    try:
+        jsonObj = nomnomnom.json()
+    except ValueError:
+        print("Received invalid JSON: "+str(nomnomnom.text()))
+        return False
     if (jsonObj['game']['careLeft'] > 0):
         wachtFF = random.randint(delayConsumptionMin, delayConsumptionMax)
         time.sleep(wachtFF)
         giveConsumption()
     return True
 
+def doClaim():
+    claimStamp = datetime.datetime.now()
+    print((claimStamp)+" Claiming!")
+    claim = requests.post(baseUrl+"/claim", headers=reqHeaders)
+    print(str(claim.json()))
+
 def checkAPI():
-    print("Checking with Kamergotchi API")
+    checkStamp = datetime.datetime.now()
+    print(str(checkStamp)+" Checking with Kamergotchi API")
     check = requests.get(baseUrl, headers=reqHeaders)
     jsonObj = check.json()
     resetTime = dateutil.parser.parse(str(jsonObj['game']['careReset']))
+    resetClaim = dateutil.parser.parse(str(jsonObj['game']['claimReset']))
+    resetClaimTS = round(resetClaim.timestamp())
     resetTS = round(resetTime.timestamp()) 
     curTS = round(time.time())
+    if (curTS - resetClaimTS > 0):
+        doClaim()
     if (curTS - resetTS > 0):
         return True
     else:
         return False
 
 def main():
-    print("Norbie's Kamergotchi Bot v1.0")
+    print("Norbie's Kamergotchi Bot v1.1")
     print("https://www.norbert.in/")
     print("-----------------------------")
     print("We're currently impersonating X-PLAYER "+xplayer)
+    # Deze while loop kan veel eleganter:
+    # checkAPI returned care/claim reset, in var stoppen en vergelijken ipv elke 60 sec te pollen.
+    # Volgende versie jo!
     while True:
         while readyForConsumption == False:
             checkTrue = checkAPI()
